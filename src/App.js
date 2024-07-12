b467f38ca566e737fc3a8865a6283e0088c6fa29
Can Ngo <44318634+nncan99@users.noreply.github.com>
2024-07-12 19:34:57 +0700

    Modified some styles

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     breaks: true
   });
   
-  let initialEditor = "# Welcome to my React Markdown Previewer!\n\n## This is a sub-heading...\n### And here's some other cool stuff:\n\nHeres some code, `<div></div>`, between 2 backticks.\n\n```\n// this is multi-line code:\n\nfunction anotherExample(firstLine, lastLine) {\n  if (firstLine == '```' && lastLine == '```') {\n    return multiLineCode;\n  }\n}\n```\n\nYou can also make text **bold**... whoa!\nOr _italic_.\nOr... wait for it... **_both!_**\nAnd feel free to go crazy ~~crossing stuff out~~.\n\nThere's also [links](https://www.freecodecamp.org), and\n> Block Quotes!\n\nAnd if you want to get really crazy, even tables:\n\nWild Header | Crazy Header | Another Header?\n------------ | ------------- | -------------\nYour content can | be here, and it | can be here....\nAnd here. | Okay. | I think we get it.\n\n- And of course there are lists.\n  - Some are bulleted.\n     - With different indentation levels.\n        - That look like this.\n\n\n1. And there are numbered lists too.\n1. Use just 1s if you want!\n1. And last but not least, let's not forget embedded images:\n\n![freeCodeCamp Logo]\n"
+  let initialEditor = "# Welcome to my React Markdown Previewer!\n\n## This is a sub-heading...\n### And here's some other cool stuff:\n\nHeres some code, `<div></div>`, between 2 backticks.\n\n```\n// this is multi-line code:\n\nfunction anotherExample(firstLine, lastLine) {\n  if (firstLine == '```' && lastLine == '```') {\n    return multiLineCode;\n  }\n}\n```\n\nYou can also make text **bold**... whoa!\nOr _italic_.\nOr... wait for it... **_both!_**\nAnd feel free to go crazy ~~crossing stuff out~~.\n\nThere's also [links](https://www.freecodecamp.org), and\n> Block Quotes!\n\nAnd if you want to get really crazy, even tables:\n\nWild Header | Crazy Header | Another Header?\n------------ | ------------- | -------------\nYour content can | be here, and it | can be here....\nAnd here. | Okay. | I think we get it.\n\n- And of course there are lists.\n  - Some are bulleted.\n     - With different indentation levels.\n        - That look like this.\n\n\n1. And there are numbered lists too.\n1. Use just 1s if you want!\n1. And last but not least, let's not forget embedded images:\n\n![freeCodeCamp Logo](https://cataas.com/cat)\n"
 
   let initialPreview = marked(initialEditor);
   
@@ -66,8 +66,8 @@ function App() {
           <div id="editor-container">
             <div id="nav-editor">
               <span>Editor</span>
-              <i id="icon-expand-editor" class="fas fa-expand-arrows-alt" onClick={this.expandEditor}></i>
-              <i id="icon-minimize-editor" class="fas fa-compress-alt hide" onClick={this.minimizeEditor}></i>
+              <i id="icon-expand-editor" className="fas fa-expand-arrows-alt" onClick={this.expandEditor}></i>
+              <i id="icon-minimize-editor" className="fas fa-compress-alt hide" onClick={this.minimizeEditor}></i>
             </div>
             <textarea 
               id="editor" 
@@ -80,8 +80,8 @@ function App() {
           <div id="previewer-container">
             <div id="nav-previewer">
             <span>Previewer</span>
-            <i id="icon-expand-previewer" class="fas fa-expand-arrows-alt" onClick={this.expandPreviewer}></i>
-            <i id="icon-minimize-previewer" class="fas fa-compress-alt hide" onClick={this.minimizePreviewer}></i>
+            <i id="icon-expand-previewer" className="fas fa-expand-arrows-alt" onClick={this.expandPreviewer}></i>
+            <i id="icon-minimize-previewer" className="fas fa-compress-alt hide" onClick={this.minimizePreviewer}></i>
             </div>
             <div id="preview" dangerouslySetInnerHTML={{__html: this.state.preview}}></div>
           </div>
